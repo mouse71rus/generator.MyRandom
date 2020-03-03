294999fb9d8f14ef9e6d9bdc959e71eef2063406
mouse71rus <zhimerin98007@gmail.com>
2020-03-03 22:52:02 +0300

    Удалены лишние файлы

--- views/layouts/head.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="utf-8">
-    <meta name="description" content="Авторизация с помощью API">
-    <meta name="keywords" content="API, project"><!--Ключевые слова для поисковых систем-->
-    <link rel="shortcut icon" href="/template/images/icon.png" type="image/png">
-	<link rel="stylesheet" href="/template/font-awesome/css/font-awesome.min.css">
-    <link href="/template/css/style.css" rel="stylesheet" type="text/css">
